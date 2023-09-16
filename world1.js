69c415f5cbd7c08bd2aebcb2de44e0ba76b8006a
Irving Delgado <irvingdelgado6@gmail.com>
2023-09-16 17:36:40 -0600

    world1 code added

---
+++ world1.js
@@ -0,0 +1,47 @@
+class program {
+    program() {
+
+        move();
+        turnleft();
+        turnleft();
+        turnleft();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        turnleft();
+        move();
+        turnleft();
+        turnleft();
+        turnleft();
+        move();
+        move();
+        move();
+
+        pickbeeper();
+        turnleft();
+        move();
+
+        turnleft();
+        turnleft();
+        turnleft();
+        move();
+        move();
+        putbeeper();
+        move();
+        turnleft();
+        move();
+        turnoff();
+    }
+}
